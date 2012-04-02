2ad13aad1086bd1886a8d29bfbf4e65ddbcadf20
Juozas Kaziukenas <juozas@juokaz.com>
2012-04-02 17:35:45 +0100

    Make automatically

--- make.sh
+++ make.sh
@@ -1,4 +1,24 @@
+#!/bin/sh
 
-latex dissertation.tex
-dvips dissertation
-ps2pdf dissertation.ps
+check() {
+    dir="."
+    chsum1=`ls -l | grep .tex | sha1sum | awk '{print $1}'`
+    chsum2=$chsum1
+
+	while :
+    do
+	    while [ $chsum1 = $chsum2 ]
+	    do
+	        sleep 1
+	        chsum2=`ls -l | grep .tex | sha1sum | awk '{print $1}'`
+	    done
+
+    	chsum1=`ls -l | grep .tex | sha1sum | awk '{print $1}'`
+
+	    latex dissertation.tex > /dev/null
+		dvips dissertation > /dev/null
+		ps2pdf dissertation.ps > /dev/null
+	done
+}
+
+check $*
